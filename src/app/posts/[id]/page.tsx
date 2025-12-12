43fdcc032aec723a3fff8a13dcfd60cebda91226
kimhss <hyunuary108@kyonggi.ac.kr>
2025-12-12 17:36:34 +0900

    50강 work

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -75,11 +75,32 @@ function usePostComments(id: number) {
     });
   };
 
+  const modifyComment = (
+    commentId : number,
+    content : string,
+    onSuccess: (data : any) => void
+  ) => {
+    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
+      method: "PUT",
+      body: JSON.stringify({ content })
+    }). then((data) => {
+      if (postComments == null) return;
+
+      setPostComments(
+        postComments.map((comment) => 
+          comment.id === commentId ? { ...comment, content } : comment)
+      );
+      
+      onSuccess(data);
+    });
+  };
+
   return {
     id,
     postComments,
     deleteComment,
     writeComment,
+    modifyComment
   };
 }
 
@@ -218,7 +239,7 @@ function PostCommentListItem({
   postCommentsState: ReturnType<typeof usePostComments>;
 }) {
   const [modifyMode, setModifyMode] = useState(false);
-  const { deleteComment: _deleteComment } = postCommentsState;
+  const { deleteComment: _deleteComment, modifyComment } = postCommentsState;
 
   const toggleModifyMode = () => {
     setModifyMode(!modifyMode);
@@ -232,6 +253,35 @@ function PostCommentListItem({
     });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const contentTextarea = form.elements.namedItem(
+      "content"
+    ) as HTMLTextAreaElement;
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("댓글 내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    if (contentTextarea.value.length < 2) {
+      alert("댓글 내용을 2자 이상 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    modifyComment(comment.id, contentTextarea.value, (data) => {
+      alert(data.msg);
+      toggleModifyMode();
+    });
+  };
+
   return (
     <li className="flex gap-2 items-center">
       <span>{comment.id} : </span>
@@ -239,7 +289,7 @@ function PostCommentListItem({
       {modifyMode && (
         <form
           className="flex gap-2 items-center"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={handleSubmit}
         >
           <textarea
             className="border p-2 rounded"
@@ -248,6 +298,7 @@ function PostCommentListItem({
             maxLength={100}
             rows={5}
             defaultValue={comment.content}
+            autoFocus
           />
           <button className="p-2 rounded border" type="submit">
             수정
