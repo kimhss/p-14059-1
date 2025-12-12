6c4c9d1d435ce84391317e93b83ad1d7b0a4cbe7
kimhss <hyunuary108@kyonggi.ac.kr>
2025-12-12 17:23:55 +0900

    49강 work

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -196,7 +196,7 @@ function PostCommentList({
       )}
 
       {postComments != null && postComments.length > 0 && (
-        <ul>
+        <ul className="mt-2 flex flex-col gap-2">
           {postComments.map((comment) => (
             <PostCommentListItem
               key={comment.id}
@@ -210,7 +210,6 @@ function PostCommentList({
   );
 }
 
-
 function PostCommentListItem({
   comment,
   postCommentsState,
@@ -218,7 +217,12 @@ function PostCommentListItem({
   comment: PostCommentDto;
   postCommentsState: ReturnType<typeof usePostComments>;
 }) {
+  const [modifyMode, setModifyMode] = useState(false);
   const { deleteComment: _deleteComment } = postCommentsState;
+
+  const toggleModifyMode = () => {
+    setModifyMode(!modifyMode);
+  };
 
   const deleteComment = (commentId: number) => {
     if (!confirm(`${commentId}번 댓글을 정말로 삭제하시겠습니까?`)) return;
@@ -229,8 +233,30 @@ function PostCommentListItem({
   };
 
   return (
-    <li>
-      {comment.id} : {comment.content}
+    <li className="flex gap-2 items-center">
+      <span>{comment.id} : </span>
+      {!modifyMode && <span>{comment.content}</span>}
+      {modifyMode && (
+        <form
+          className="flex gap-2 items-center"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <textarea
+            className="border p-2 rounded"
+            name="content"
+            placeholder="댓글 내용"
+            maxLength={100}
+            rows={5}
+            defaultValue={comment.content}
+          />
+          <button className="p-2 rounded border" type="submit">
+            수정
+          </button>
+        </form>
+      )}
+      <button className="p-2 rounded border" onClick={toggleModifyMode}>
+        {modifyMode ? "수정취소" : "수정"}
+      </button>
       <button
         className="p-2 rounded border"
         onClick={() => deleteComment(comment.id)}
@@ -240,7 +266,6 @@ function PostCommentListItem({
     </li>
   );
 }
-
 
 function PostCommentWriteAndList({
   postCommentsState,
